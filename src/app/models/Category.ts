b0fd14f667d89bd59af7a829e6c040eef0a1fb74
Breno Guilherme de Souza Lopes <brenogdev@gmail.com>
2022-11-18 23:49:32 -0300

    (feat): criado models iniciais da aplicação

---
+++ src/app/models/Category.ts
@@ -0,0 +1,16 @@
+/* eslint-disable linebreak-style */
+import { model, Schema } from 'mongoose';
+
+export const Category = model(
+  'Category',
+  new Schema({
+    name: {
+      type: String,
+      required: true,
+    },
+    icon: {
+      type: String,
+      required: true,
+    },
+  })
+);
---
+++ src/app/models/Order.ts
@@ -0,0 +1,36 @@
+/* eslint-disable linebreak-style */
+import { model, Schema } from 'mongoose';
+
+export const Order = model(
+  'Order',
+  new Schema({
+    table: {
+      type: String,
+      required: true,
+    },
+    status: {
+      type: String,
+      enum: ['WATING', 'IN_PRODUCTION', 'DONE'],
+      default: 'WATING',
+
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now
+    },
+    products: {
+      required: true,
+      type: [{
+        product: {
+          type: Schema.Types.ObjectId,
+          required: true,
+          ref: 'Product',
+        },
+        quantity: {
+          type: Number,
+          default: 1
+        }
+      }]
+    }
+  })
+);
